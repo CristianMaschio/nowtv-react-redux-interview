a6b342742e340e136e394b64c2c89569e0b7d793
CristianMaschio <blackspartan@hotmail.it>
2019-10-24 09:21:38 +0100

    [Home-components] add Message

---
+++ src/containers/components/message.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './message.scss'
+
+class Message extends React.PureComponent {
+  state = {
+    isHover: false
+  }
+
+  handleIsHover = isHover => {
+    this.setState({ isHover })
+  }
+
+  render() {
+    const { style, message, user } = this.props
+    const { isHover } = this.state
+
+    return (
+      <div
+        style={style}
+        onMouseEnter={() => this.handleIsHover(true)}
+        onMouseLeave={() => this.handleIsHover(false)}>
+        <div className="message-container">
+          <div className={`message-detail ${isHover ? 'is-hover' : ''}`}>
+            <span>{user.email}</span>
+          </div>
+          <img className="avatar" src={user.avatar} alt="user avatar" />
+          <div className="message-text">{message.message}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Message.propTypes = {
+  message: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired,
+    userId: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired
+  }),
+  user: PropTypes.shape({
+    avatar: PropTypes.string,
+    email: PropTypes.string,
+    firstName: PropTypes.string,
+    id: PropTypes.string,
+    ip: PropTypes.string,
+    lastName: PropTypes.string
+  }),
+  style: PropTypes.object
+}
+
+export default Message
